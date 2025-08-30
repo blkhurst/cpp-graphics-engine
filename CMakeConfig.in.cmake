4935797079802e697785834bc2ade8a3527407ff
Blkhurst <github@blkhurst.co.uk>
2025-08-30 19:44:01 +0100

    feat(cmake): add GLM dependency to CMakeLists and CMakeConfig

--- CMakeConfig.in.cmake
+++ CMakeConfig.in.cmake
@@ -3,6 +3,7 @@
 include(CMakeFindDependencyMacro)
 
 find_dependency(OpenGL REQUIRED)
+find_dependency(glm 1.0.1 REQUIRED)
 find_dependency(glfw3 3.4 REQUIRED)
 find_dependency(spdlog 1.15.3 REQUIRED)
 
