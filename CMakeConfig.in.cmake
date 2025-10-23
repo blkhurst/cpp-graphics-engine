e86b430a60a2eb3a1f2836f59d25eae2250a497c
Blkhurst <github@blkhurst.co.uk>
2025-10-23 16:17:25 +0100

    build(cmake) add assimp 6.0.2
    - Ensure FetchContent packages have their BUILD_INSTALL set to true to
      resolve any warnings about missing from export set.

--- CMakeConfig.in.cmake
+++ CMakeConfig.in.cmake
@@ -6,5 +6,6 @@ find_dependency(OpenGL REQUIRED)
 find_dependency(glm 1.0.1 REQUIRED)
 find_dependency(glfw3 3.4 REQUIRED)
 find_dependency(spdlog 1.15.3 REQUIRED)
+find_dependency(assimp 6.0.2 REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/BlkhurstEngineTargets.cmake")
